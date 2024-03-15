1e8283de89556b228eb875f7d931c7892fe3cf34
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2024-03-16 01:38:08 +0700

    Run make lint

--- contracts/proxy/QuadraticLendCompoundProxy.sol
+++ contracts/proxy/QuadraticLendCompoundProxy.sol
@@ -5,5 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract QuadraticLendCompoundProxy is TransparentUpgradeableProxy {
-    constructor(address _contractAddress) TransparentUpgradeableProxy(_contractAddress, msg.sender, "") {}
+    constructor(
+        address _contractAddress
+    ) TransparentUpgradeableProxy(_contractAddress, msg.sender, "") {}
 }
