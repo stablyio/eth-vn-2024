404d56c0dca675e5ff164d9f83afaaecff53158d
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2024-03-16 01:05:41 +0700

    Modified all proxy contracts to use _contractAddress params for contructor

--- contracts/proxy/QuadraticBorrowCompoundProxy.sol
+++ contracts/proxy/QuadraticBorrowCompoundProxy.sol
@@ -5,8 +5,5 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract QuadraticBorrowCompoundProxy is TransparentUpgradeableProxy {
-    address private constant initProxy =
-        0xf50Db1CE7924e860a712e186A794BA61dbDB9D58;
-
-    constructor() TransparentUpgradeableProxy(initProxy, msg.sender, "") {}
+    constructor(address _contractAddress) TransparentUpgradeableProxy(_contractAddress, msg.sender, "") {}
 }
--- contracts/proxy/QuadraticLendCompoundProxy.sol
+++ contracts/proxy/QuadraticLendCompoundProxy.sol
@@ -5,8 +5,5 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract QuadraticLendCompoundProxy is TransparentUpgradeableProxy {
-    address private constant initProxy =
-        0x08cE6fED8EC9BaB309E1E60c215DcaDDb27A5791;
-
-    constructor() TransparentUpgradeableProxy(initProxy, msg.sender, "") {}
+    constructor(address _contractAddress) TransparentUpgradeableProxy(_contractAddress, msg.sender, "") {}
 }
