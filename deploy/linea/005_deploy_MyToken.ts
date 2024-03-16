06a9d74cb21f5eac174cc4bce2eabb6ab9828df6
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2024-03-16 17:14:48 +0700

    Only deploy MyToken on local node

--- deploy/linea/005_deploy_MyToken.ts
+++ deploy/linea/005_deploy_MyToken.ts
@@ -3,13 +3,18 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { deployContract } from "../../utils/deploy";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  // Deploy the core contract
-  await deployContract(
-    "MyToken",
-    [10000000000],
-    undefined,
-    hre,
-  );
+  const { network } = hre;
+  if (network.name == "hardhat") {
+    // Deploy the core contract
+    await deployContract(
+      "MyToken",
+      [10000000000],
+      undefined,
+      hre,
+    );
+  } else {
+    console.log("MyToken is not deployed on non-local networks");
+  }
 };
 export default func;
 func.tags = ["MyToken"];
